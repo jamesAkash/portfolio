af1137e38bc5399b13b8362216e7d97a63535ec6
Bamwamm <akash.james@outlook.com>
2022-12-22 15:00:14 +0530

    fully functioning dynamic menu and pages created

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,22 +1,25 @@
 import "./menu.scss";
 
-export default function Menu() {
+export default function Menu({ menuOpen, setMenuOpen }) {
+  const close = () => {
+    setMenuOpen(false);
+  };
   return (
-    <div className="menu">
+    <div className={`menu ${menuOpen && "active"}`}>
       <ul>
-        <li>
+        <li onClick={close}>
           <a href="#intro">Home</a>
         </li>
-        <li>
+        <li onClick={close}>
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li>
+        <li onClick={close}>
           <a href="#works">Works</a>
         </li>
-        <li>
+        <li onClick={close}>
           <a href="#testimonials">Testimonials</a>
         </li>
-        <li>
+        <li onClick={close}>
           <a href="#contact">Contact</a>
         </li>
       </ul>
