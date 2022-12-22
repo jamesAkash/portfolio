232d6015b6f636321e8c7a704943292792e38fbd
Bamwamm <akash.james@outlook.com>
2022-12-22 08:32:38 +0530

    Made all components and added to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import Testimonials from "./components/testimonials/Testimonials";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
